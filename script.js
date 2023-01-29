d0ad6a8d0b89691d4838297379196a2aa62567a7
codesoup <82330389+helikopterelidojosa@users.noreply.github.com>
2023-01-29 23:00:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var guessThisNumber = Math.ceil(Math.random()*1000000)
+var guessScore = 0;
+
+
+function findNumberGame(){
+guessScore++;
+
+
+var myNumber  = document.getElementById('myGuess').value;
+
+if(myNumber == guessThisNumber){
+    document.getElementById('info').innerHTML = "<br>Congratulations, You guessed right " +guessThisNumber+ " <br> It took you " + guessScore + " guesses";
+    document.getElementById('history').innerHTML +='<br><b style="color:cyan">'+ myNumber + " Right Number!</b>";
+
+}
+
+if(myNumber < guessThisNumber){
+    document.getElementById('history').innerHTML +='<br><b style="color:green">'+ myNumber + " More!</b>";
+    document.getElementById('info').innerHTML =""+ myNumber +" More!";
+}
+
+
+if(myNumber > guessThisNumber){
+    document.getElementById('history').innerHTML +='<br><b style="color:red">'+ myNumber + " Less!</b>";
+    document.getElementById('info').innerHTML =""+ myNumber +" Less!";;
+}
+}
